a4182b23e484825cffe2e861c98361fd750c33d8
Roman Kulkov <romawka2410@gmail.com>
2023-11-30 19:00:57 +0200

    play1

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"myapp/rps"
 	"net/http"
 	"text/template"
 )
@@ -10,7 +11,15 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html")
 }
 
+func playRound(w http.ResponseWriter, r *http.Request) {
+
+	winner, computerChoice, roudResult := rps.PlayRound(1)
+	log.Println(winner, computerChoice, roudResult)
+
+}
+
 func main() {
+	http.HandleFunc("/play", playRound)
 	http.HandleFunc("/", homePage)
 
 	log.Println("Starting web server on port 8080")
